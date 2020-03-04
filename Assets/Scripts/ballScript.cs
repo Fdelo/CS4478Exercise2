410c64e388f0c85a274715df1e5b11ae4cd34831
Francis Delorenzi <fdeloren@lakeheadu.ca>
2020-03-03 20:48:31 -0500

    Player Movement and Ball Collision working

---
+++ Assets/Scripts/ballScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballScript : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private Vector2 velocity = new Vector2(0.0f, -0.5f);
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D.gravityScale = 0.0f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+      
+      velocity.y = velocity.y * -1;
+    }
+
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      
+        rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/playermovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private float revSpeed = 50.0f;
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        // transform.localScale = new Vector3(3.0f, 3.0f, 3.0f);
+        rb2D.gravityScale = 0.0f;
+    }
+
+    void FixedUpdate()
+    {
+      //Input.GetAxisRaw("horizontal");
+      if (Input.GetKey(KeyCode.A))
+      {
+        rb2D.MoveRotation(rb2D.rotation - revSpeed * Time.fixedDeltaTime);  
+      }
+      else if (Input.GetKey(KeyCode.D))
+      {
+        rb2D.MoveRotation(rb2D.rotation + revSpeed * Time.fixedDeltaTime);  
+      }
+        
+    }
+}
